50f7819516f32d683896a78ad9f38ff19886e352
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-06-24 11:25:25 +0200

    Created new folder, added bootstrap and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer> <a href="https://github.com/tania-garcia-gallego">
+          Open source code
+        </a>  by Tania Garcia Gallego 👩‍💻</footer>
+      </div>
+    </div >
   );
 }
 
