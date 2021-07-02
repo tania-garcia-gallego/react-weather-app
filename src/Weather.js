c28060607b03edbfa4fee725f17f7d1df0f13f66
tania-garcia-gallego <75450950+tania-garcia-gallego@users.noreply.github.com>
2021-07-02 13:12:32 +0200

    added https

--- src/Weather.js
+++ src/Weather.js
@@ -25,7 +25,7 @@ export default function Weather(props) {
 
     function search(){
          const apiKey = "751305c75f9526727cdf4f36e45a4e75";
-         let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+         let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
          axios.get(apiUrl).then(handleResponse);
     }
 
